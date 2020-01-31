31611bae9506ac8b61e65d3477e711689bffcce8
Alberto González Balderas <albertoglezb@hotmail.com>
2020-01-31 17:12:38 -0600

    Se agrego la funcionalidad de buscar por Id. Se termino la clase: 'Implementacion de findbyId y pruebas en el servicio Agent.'

--- platziverse-db/test/agent-test.js
+++ platziverse-db/test/agent-test.js
@@ -27,6 +27,12 @@ test.beforeEach(async () => {
     hasMany: sandbox.spy()
   };
 
+  // Model findById Stub
+  AgentStub.findById = sandbox.stub();
+  AgentStub.findById
+    .withArgs(id)
+    .returns(Promise.resolve(agentFixtures.byId(id)));
+
   const setupDatabase = proxyquire("../", {
     "./models/agent": () => AgentStub,
     "./models/metric": () => MetricStub
@@ -57,5 +63,13 @@ test.serial("Setup", t => {
 
 test.serial("Agent#findById", async t => {
   let agent = await db.Agent.findById(id);
+
+  t.true(AgentStub.findById.called, "findById shold be called on model");
+  t.true(AgentStub.findById.calledOnce, "findById should be called once");
+  t.true(
+    AgentStub.findById.calledWith(id),
+    "findById shold be called with specidified id"
+  );
+
   t.deepEqual(agent, agentFixtures.byId(id), "should be the same");
 });
