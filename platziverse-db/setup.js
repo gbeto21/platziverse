54c20400bb7208ed8b4178891280998f1c85c9c3
Alberto González Balderas <albertoglezb@hotmail.com>
2020-02-03 16:41:45 -0600

    Se resolvio el reto de pasar un parametro para saltarse la validacion de que si se quiere eliminar la base de datos. Se termino la clase: 'Reto: como modificar el script de inicializacion de la base de datos.'

--- platziverse-db/setup.js
+++ platziverse-db/setup.js
@@ -1,45 +1,55 @@
-'use strict'
+"use strict";
 
-const debug = require('debug')('platziverse:db:setup')
-const inquirer = require('inquirer')
-const chalk = require('chalk')
-const db = require('./')
+const debug = require("debug")("platziverse:db:setup");
+const inquirer = require("inquirer");
+const chalk = require("chalk");
+const db = require("./");
 
-const prompt = inquirer.createPromptModule()
+const prompt = inquirer.createPromptModule();
 
-async function setup () {
-  const answer = await prompt([
-    {
-      type: 'confirm',
-      name: 'setup',
-      message: 'This will destroy the database, Are your sure? '
+async function setup() {
+  const flagYes = process.argv.indexOf("yes");
+
+  if (flagYes) {
+    deleteDataBase();
+  } else {
+    const answer = await prompt([
+      {
+        type: "confirm",
+        name: "setup",
+        message: "This will destroy the database, Are your sure? "
+      }
+    ]);
+
+    if (!answer.setup) {
+      return console.log("Nothing happened :) ");
     }
-  ])
 
-  if (!answer.setup) {
-    return console.log('Nothing happened :) ')
+    deleteDataBase();
   }
+}
 
+async function deleteDataBase() {
   const config = {
-    database: process.env.DB_NAME || 'platziverse',
-    username: process.env.DB_USER || 'platzi',
-    password: process.env.DB_PASS || 'platzi',
-    host: process.env.DB_HOST || 'localhost',
-    dialect: 'postgres',
+    database: process.env.DB_NAME || "platziverse",
+    username: process.env.DB_USER || "platzi",
+    password: process.env.DB_PASS || "platzi",
+    host: process.env.DB_HOST || "localhost",
+    dialect: "postgres",
     loggin: s => debug(s),
     setup: true
-  }
+  };
 
-  await db(config).catch(handleFatalError)
+  await db(config).catch(handleFatalError);
 
-  console.log('Succes')
-  process.exit(0)
+  console.log("Succes");
+  process.exit(0);
 }
 
-function handleFatalError (err) {
-  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
-  console.error(err.stac)
-  process.exit(1)
+function handleFatalError(err) {
+  console.error(`${chalk.red("[fatal error]")} ${err.message}`);
+  console.error(err.stac);
+  process.exit(1);
 }
 
-setup()
+setup();
