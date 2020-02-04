6726a065974f94312fc70248a92b310551a81b84
Alberto González Balderas <albertoglezb@hotmail.com>
2020-02-04 08:20:32 -0600

    Se agrego la dependencia para acceder a la base de datos desde el servidor. Se termino la clase: 'Como integrar el servidor MQTT con la base de datos'.

--- platziverse-mqtt/server.js
+++ platziverse-mqtt/server.js
@@ -1,47 +1,64 @@
-"use strict";
+'use strict'
 
-const debug = require("debug")("platziverse:mqtt");
-const mosca = require("mosca");
-const redis = require("redis");
-const chalk = require("chalk");
+const debug = require('debug')('platziverse:mqtt')
+const mosca = require('mosca')
+const redis = require('redis')
+const chalk = require('chalk')
+const db = require('platziverse-db')
 
 const backend = {
-  type: "redis",
+  type: 'redis',
   redis,
   return_buffers: true
-};
+}
 
 const settings = {
   port: 1883,
   backend
-};
-
-const server = new mosca.Server(settings);
-
-server.on("clientConnected", client => {
-  debug(`Client Connected: ${client.id}`);
-});
-
-server.on("clientDisconnected", client => {
-  debug(`Client Disconnected: ${client.id}`);
-});
-
-server.on("published", (packet, client) => {
-  debug(`Received: ${packet.topic}`);
-  debug(`Payload: ${packet.payload}`);
-});
-
-server.on("ready", () => {
-  console.log(`${chalk.green("[platziverse-mqtt]")} server is running`);
-});
-
-server.on("error", handleFatalError);
-
-function handleFatalError(err) {
-  console.error(`${chalk.red("[fatal error]")} ${err.message}`);
-  console.error(err.stack);
-  process.exit(1);
 }
 
-process.on("uncaughtException", handleFatalError);
-process.on("unhandledRejection", handleFatalError);
+const config = {
+  database: process.env.DB_NAME || 'platziverse',
+  username: process.env.DB_USER || 'platzi',
+  password: process.env.DB_PASS || 'platzi',
+  host: process.env.DB_HOST || 'localhost',
+  dialect: 'postgres',
+  loggin: s => debug(s)
+}
+
+const server = new mosca.Server(settings)
+
+let Agent, Metric
+
+server.on('clientConnected', client => {
+  debug(`Client Connected: ${client.id}`)
+})
+
+server.on('clientDisconnected', client => {
+  debug(`Client Disconnected: ${client.id}`)
+})
+
+server.on('published', (packet, client) => {
+  debug(`Received: ${packet.topic}`)
+  debug(`Payload: ${packet.payload}`)
+})
+
+server.on('ready', async () => {
+  const services = await db(config).catch(handleFatalError)
+
+  Agent = services.Agent
+  Metric = services.Metric
+
+  console.log(`${chalk.green('[platziverse-mqtt]')} server is running`)
+})
+
+server.on('error', handleFatalError)
+
+function handleFatalError (err) {
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+process.on('uncaughtException', handleFatalError)
+process.on('unhandledRejection', handleFatalError)
