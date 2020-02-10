4d194cac9a734a3191d384a56c81c5b0e45571ac
Alberto González Balderas <albertoglezb@hotmail.com>
2020-02-10 16:20:20 -0600

    Se agrego el manejo de errores. Se termino la clase: 'Implementacion de manejo de errores con express'

--- platziverse-api/api.js
+++ platziverse-api/api.js
@@ -9,8 +9,11 @@ api.get("/agents", (req, res) => {
   debug("A request has come to /agent");
   res.send({});
 });
-api.get("/agent/:uuid", (req, res) => {
+api.get("/agent/:uuid", (req, res, next) => {
   const { uuid } = req.params;
+  if (uuid !== "yyy") {
+    return next(new Error("Agent not found."));
+  }
   res.send({ uuid });
 });
 
