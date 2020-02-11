2cda5721900a04cba0306d4b4da17a765cbbeb9c
Alberto González Balderas <albertoglezb@hotmail.com>
2020-02-11 11:27:49 -0600

    Se implementaron las funcionalidades restantes de las rutas de la api. Se termino la clase: 'Implementación de ruta del API.'

--- platziverse-api/api.js
+++ platziverse-api/api.js
@@ -10,7 +10,7 @@ let services, Agent, Metric;
 
 api.use("*", async (req, res, next) => {
   if (!services) {
-    debug('Connectin to database')
+    debug("Connectin to database");
     try {
       services = await db(config.db);
     } catch (error) {
@@ -22,27 +22,71 @@ api.use("*", async (req, res, next) => {
   next();
 });
 
-api.get("/agents", (req, res) => {
+api.get("/agents", async (req, res, next) => {
   debug("A request has come to /agent");
-  res.send({});
-  // res.status(201).send({});
-});
-api.get("/agent/:uuid", (req, res, next) => {
-  const { uuid } = req.params;
-  if (uuid !== "yyy") {
-    return next(new Error("Agent not found."));
+  let agents = [];
+
+  try {
+    agents = await Agent.findConnected();
+  } catch (error) {
+    return next(error);
   }
-  res.send({ uuid });
-});
 
-api.get("/metrics/:uuid", (req, res) => {
+  res.send(agents);
+});
+api.get("/agent/:uuid", async (req, res, next) => {
   const { uuid } = req.params;
-  res.send({ uuid });
+
+  debug(`request to /aent/${uuid}`);
+
+  let agent;
+  try {
+    agent = await Agent.findByUuid(uuid);
+  } catch (error) {
+    return next(error);
+  }
+
+  if (!agent) {
+    return next(new Error(`Agent not found with uuid ${uuid}.`));
+  }
+  res.send(agent);
 });
 
-api.get("/metrics/:uuid/:type", (req, res) => {
+api.get("/metrics/:uuid", async (req, res, next) => {
+  const { uuid } = req.params;
+
+  debug(`request to /metrics/${uuid}`);
+
+  let metrics = [];
+
+  try {
+    metrics = await Metric.getByAgenteUuid(uuid);
+  } catch (error) {
+    next(error);
+  }
+
+  if (!metrics || metrics.length === 0) {
+    return next(new Error(`Metrics not found for agent with uuid ${uuid}`));
+  }
+
+  res.send(metrics);
+});
+
+api.get("/metrics/:uuid/:type", async (req, res, next) => {
   const { uuid, type } = req.params;
-  res.send({ uuid, type });
+
+  debug(`request to /metrics/${uuid}/${type}`);
+  let metrics = [];
+  try {
+    metrics = await Metric.getByAgenteType(type, uuid);
+  } catch (error) {
+    return next(error);
+  }
+  if (!metrics || metrics.length === 0) {
+    return next(new Error(`Metrics (${type}) not found for agent with uuid ${uuid}`));
+  }
+
+  res.send(metrics);
 });
 
 module.exports = api;
