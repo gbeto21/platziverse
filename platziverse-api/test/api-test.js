3fd41ef515730f221711505cf8a8ea0834ae0bd8
Alberto González Balderas <albertoglezb@hotmail.com>
2020-02-12 09:21:48 -0600

    Se agrego el token en el archivo de pruebas. Se termino la clase: 'Modificando las pruebas de integracion.'

--- platziverse-api/test/api-test.js
+++ platziverse-api/test/api-test.js
@@ -1,15 +1,18 @@
 "use strict";
 
 const test = require("ava");
+const util = require("util");
 const request = require("supertest");
 const sinon = require("sinon");
 const proxyquire = require("proxyquire");
-
+const config = require("../config");
 const agentFixtures = require("./fixtures/agent");
-
+const auth = require("../auth");
+const sign = util.promisify(auth.sign);
 let sandbox = null;
 let server = null;
 let dbStub = null;
+let token = null;
 let AgentStub = {};
 let MetricStub = {};
 
@@ -25,6 +28,8 @@ test.beforeEach(async () => {
 
   AgentStub.findConnected = sandbox.stub();
   AgentStub.findConnected.returns(Promise.resolve(agentFixtures.connected));
+
+  token = await sign({ admin: true, username: "platzi" }, config.auth.secret);
 
   const api = proxyquire("../api", {
     "platziverse-db": dbStub
@@ -42,6 +47,7 @@ test.afterEach(() => {
 test.serial.cb("/api/agents", t => {
   request(server)
     .get("/api/agents")
+    .set("Authorization", `Bearer ${token}`)
     .expect(200)
     .expect("Content-Type", /json/)
     .end((err, res) => {
